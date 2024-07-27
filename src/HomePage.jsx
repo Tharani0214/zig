dc8b2eca741f57ce27268159f1ff02f4eff04cb5
Tharani0214 <tharanisri0214@gmail.com>
2024-07-27 13:19:07 +0530

    initial

---
+++ src/HomePage.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; // Import the CSS file
+import './components/Client.css';
+
+function HomePage() {
+  const tableStyle = {
+    backgroundColor: '#b3b3ff', // Set the background-color property
+    // Add other style properties as needed
+  };
+
+
+  return (
+    <div style={{ position: 'relative'}}>
+      <h1>Client Data from API</h1>
+      
+      <div className="home-container">
+        
+
+         <div className="center" style={{paddingLeft:450}}>
+        <table style={tableStyle}>
+                <tr> 
+                <th>CLIENT ID</th> 
+                <th>CLIENT NAME</th>
+                <th>VIEW</th>
+                </tr> 
+                 
+               
+              
+                <tr> 
+                <td>54</td> 
+                <td>Isabella County Transportation</td>
+                <Link to="/Page4">
+                <td> <button type="button" className="btn btn-outline-view">
+                view
+              </button> </td> 
+              </Link> 
+                </tr>
+               
+                <tr> 
+                <td>51</td> 
+                <td>Marquette</td>
+                <Link to="/Page6">
+                <td> <button type="button" className="btn btn-outline-view">
+                view
+              </button> </td> 
+              </Link> 
+                </tr>
+                <tr> 
+               <td>50</td> 
+               <td>Schoolcraft</td>
+               <Link to="/Page7">
+                <td> <button type="button" className="btn btn-outline-view">
+                view
+              </button> </td> 
+              </Link>
+               </tr>
+               <tr> 
+               <td>49</td> 
+                 <td>Clinton Area Transit</td>
+                 <Link to="/Page8">
+                <td> <button type="button" className="btn btn-outline-view">
+                view
+              </button> </td> 
+              </Link>
+                </tr>
+                <tr> 
+                <td>34</td> 
+                <td>LETS Public Transit</td>
+                <Link to="/Page9">
+                <td> <button type="button" className="btn btn-outline-view">
+                view
+              </button> </td> 
+              </Link> 
+                </tr>
+               
+                </table>
+                </div>
+            
+        </div>
+
+      </div>
+   
+  );
+}
+
+export default HomePage;
